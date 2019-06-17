3af5b41af92160267a9a94ef615237cfb2b0d8cf
rjmaida <cruzio2004@outlook.com>
2019-06-17 13:01:54 -0300

    Adding shape to the voxel terrain using simplex noise

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -16,14 +16,14 @@ public class World : MonoBehaviour
 
     public bool genChunk;
 
+    // Use this for initialization
     void Start()
     {
-
-        for (int x = -2; x < 2; x++)
+        for (int x = -4; x < 4; x++)
         {
-            for (int y = -1; y < 1; y++)
+            for (int y = -1; y < 3; y++)
             {
-                for (int z = -1; z < 1; z++)
+                for (int z = -4; z < 4; z++)
                 {
                     CreateChunk(x * 16, y * 16, z * 16);
                 }
@@ -69,24 +69,8 @@ public class World : MonoBehaviour
         //Add it to the chunks dictionary with the position as the key
         chunks.Add(worldPos, newChunk);
 
-
-        for (int xi = 0; xi < 16; xi++)
-        {
-            for (int yi = 0; yi < 16; yi++)
-            {
-                for (int zi = 0; zi < 16; zi++)
-                {
-                    if (yi <= 7)
-                    {
-                        SetBlock(x + xi, y + yi, z + zi, new BlockGrass());
-                    }
-                    else
-                    {
-                        SetBlock(x + xi, y + yi, z + zi, new BlockAir());
-                    }
-                }
-            }
-        }
+        var terrainGen = new TerrainGen();
+        newChunk = terrainGen.ChunkGen(newChunk);
 
         newChunk.SetBlocksUnmodified();
 
